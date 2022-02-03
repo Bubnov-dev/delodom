169557970af4b8455a00565bb79b4323f2d09b34
Sascha <sasha.bubnov.k@gmail.com>
2022-02-03 11:39:07 +0300

    Add about

--- delodom/js/script.js
+++ delodom/js/script.js
@@ -1,2 +1,16 @@
 $(document).ready(function () {
+    $(".accordion-button").on('click', function(){
+        if($(this).is(".collapsed")){
+            $(this).parent().parent().removeClass('red-border');
+            console.log($(this).parent().parent())
+        }
+        else{
+            console.log(2)
+            console.log($(this).parent().parent())
+            $(".accordion-item").removeClass('red-border');
+
+            $(this).parent().parent().addClass('red-border');
+        }
+
+    })
 })
